8f2bd10e151db0c0887e2c9ea4763e1a1e32e7a6
yx-yu <101931526+yx-yu@users.noreply.github.com>
2023-05-19 09:48:21 +0800

    Delete src directory

--- src/checkpkl.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-f = open(f'D:\onedrive\OneDrive - whu.edu.cn\Desktop\myModel\data\python\python_dedupe_definitions_v2.pkl', 'rb')
-data = pickle.load(f)
-for d in data:
-    print(d)
-    break
